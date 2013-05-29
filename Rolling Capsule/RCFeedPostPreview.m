33a9fcecdab2b2afe559e5c2f79bc1ef750fccb0
Nguyen Phi Long <Zeka304@yahoo.com>
2013-05-29 18:40:21 +0800

    Added initial set up for feed page

---
+++ Rolling Capsule/RCFeedPostPreview.m
@@ -0,0 +1,29 @@
+//
+//  FeedPostPreview.m
+//  Rolling Capsule
+//
+//  Created by Nguyen Phi Long Louis on 28/05/13.
+//  Copyright (c) 2013 Fox Cradle. All rights reserved.
+//
+
+#import "RCFeedPostPreview.h"
+
+@implementation RCFeedPostPreview
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
