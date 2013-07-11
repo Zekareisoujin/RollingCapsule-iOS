cd3b6088c6ef75cd0cc0434c01a8668317a0f12a
Nguyen Phi Long <Zeka304@yahoo.com>
2013-07-11 19:49:08 +0800

    Implement dots showing location on profile view map

--- Rolling Capsule/RCUserProfileViewController.m
+++ Rolling Capsule/RCUserProfileViewController.m
@@ -39,6 +39,14 @@ NSArray  *_postPreviewElements;
 NSString *_friendStatus;
 int       _friendshipID;
 
+// Test map scale:
+double  refLong;
+double  refLat;
+double  refX;
+double  refY;
+double  minimapScaleX;
+double  minimapScaleY;
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
     if (self) {
@@ -97,8 +105,11 @@ int       _friendshipID;
     UINib *nib = [UINib nibWithNibName:cellIdentifier bundle: nil];
     [self.collectionView registerNib:nib forCellWithReuseIdentifier:cellIdentifier];
     
+    [self calculateReferenceCoordinate];
     [self asynchFetchFeeds];
     
+    /*CGPoint test = [self calculateCoordinateOnMinimapWithCoordinate:0.0 andLattitude:51.47];
+    NSLog(@"Map coordinates x is %.2f and y is %.2f", test.x, test.y);*/
 }
 
 - (void)didReceiveMemoryWarning {
@@ -132,7 +143,9 @@ int       _friendshipID;
                      MKMapPoint point = MKMapPointForCoordinate(coord);
                      NSLog(@"Long & lat: %.2f and %.2f; producing: %.2f and %.2f", test.longitude, test.latitude, point.x, point.y);
                  }
+                 
                  [_collectionView reloadData];
+                 [self drawMinimap];
              }
              
              /*if (jsonData != NULL) {
@@ -506,6 +519,56 @@ int       _friendshipID;
         for (UIView *elem in _postPreviewElements)
             elem.layer.opacity = 1.0;
     }];
+}
+
+- (void) calculateReferenceCoordinate {
+    refLong = 103.77;
+    refLat = 1.32;
+    refX = 437;
+    refY = 157;
+    
+    double refLong2 = -122.41;
+    double refLat2 = 37.79;
+    double refX2 = 40;
+    double refY2 = 88;
+    
+    CLLocationCoordinate2D refCoord = CLLocationCoordinate2DMake(refLat, refLong);
+    MKMapPoint refPoint = MKMapPointForCoordinate(refCoord);
+    CLLocationCoordinate2D refCoord2 = CLLocationCoordinate2DMake(refLat2, refLong2);
+    MKMapPoint refPoint2 = MKMapPointForCoordinate(refCoord2);
+    
+    minimapScaleX = (refX - refX2) / (refPoint.x - refPoint2.x);
+    minimapScaleY = (refY - refY2) / (refPoint.y - refPoint2.y);
+}
+
+- (CGPoint) calculateCoordinateOnMinimapWithCoordinate: (double) longitude andLattitude: (double) lattitude {
+    CLLocationCoordinate2D refCoord = CLLocationCoordinate2DMake(refLat, refLong);
+    MKMapPoint refPoint = MKMapPointForCoordinate(refCoord);
+    CLLocationCoordinate2D coord = CLLocationCoordinate2DMake(lattitude, longitude);
+    MKMapPoint point = MKMapPointForCoordinate(coord);
+    
+    CGPoint ret = CGPointMake((point.x - refPoint.x)*minimapScaleX + refX, (point.y - refPoint.y)*minimapScaleY + refY);
+    return ret;
+}
+
+- (void) drawMinimap {
+    UIImage *minimapImage = _previewWorldMap.image;
+    UIGraphicsBeginImageContext(minimapImage.size);
+    
+    [minimapImage drawAtPoint:CGPointZero];
+	CGContextRef ctx = UIGraphicsGetCurrentContext();
+	[[UIColor redColor] setStroke];
+    [[UIColor redColor] setFill];
+    
+    for (RCPost* post in _postList){
+        CGPoint drawLoc = [self calculateCoordinateOnMinimapWithCoordinate:post.longitude andLattitude:post.latitude];
+        CGRect circleRect = CGRectMake(drawLoc.x, drawLoc.y, 10, 10);
+        CGContextFillEllipseInRect(ctx, circleRect);
+    }
+    
+    UIImage *finalImage = UIGraphicsGetImageFromCurrentImageContext();
+    [_previewWorldMap setImage:finalImage];
+	UIGraphicsEndImageContext();
 }
 
 @end
