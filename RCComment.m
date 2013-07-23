87ed24e8f653cc764f17bb400f177b693ec44488
Trinh Tuan Phuong <phuong@FOX-TWO-1.local>
2013-07-23 22:30:39 +0800

    added user profile hyperlink to comment and side menu

--- RCComment.m
+++ RCComment.m
@@ -13,13 +13,14 @@
 @synthesize authorName = _authorName;
 @synthesize commentID = _commentID;
 @synthesize createdTime = _createdTime;
-
+@synthesize userID = _userID;
 - (id) initWithNSDictionary:(NSDictionary *)userData {
     self = [super init];
     if (self) {
         _authorName = (NSString *)[userData objectForKey:@"author_name"];
         _content = (NSString *)[userData objectForKey:@"content"];
         _commentID = [[userData objectForKey:@"id"] intValue];
+        _userID = [[userData objectForKey:@"user_id"] intValue];
         NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
         [formatter setDateFormat:@"yyyy-MM-dd'T'HH:mm:ss'Z'"];
         _createdTime = [formatter dateFromString:(NSString*)[userData objectForKey:@"created_at"]];
