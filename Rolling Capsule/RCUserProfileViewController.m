507403e526b3b897c1b9c0fac81aec80c6ca283d
Trinh Tuan Phuong <phuong@FOX-TWO-1.local>
2013-08-02 23:50:45 +0800

    simplified some code to update user profile

--- Rolling Capsule/RCUserProfileViewController.m
+++ Rolling Capsule/RCUserProfileViewController.m
@@ -629,15 +629,17 @@
     //                       withObject:avatarImage];
     
     __unsafe_unretained typeof(self) weakSelf = self;
-    [_profileUser setUserAvatarAsync:avatarImage completionHandler:^(UIImage* retAvatar){
-        dispatch_async(dispatch_get_main_queue(),^{
-            if (retAvatar != nil) {
-                weakSelf.userAvatarImage = retAvatar;
-                postNotification(RCInfoStringPostSuccess);
-                [weakSelf.btnAvatarImg setBackgroundImage:retAvatar forState:UIControlStateDisabled];
-            }
-        });
-    }];
+    dispatch_async(dispatch_queue_create(RCCStringAppDomain, NULL), ^{
+        [_profileUser setUserAvatar:avatarImage completionHandler:^(UIImage* retAvatar){
+            dispatch_async(dispatch_get_main_queue(),^{
+                if (retAvatar != nil) {
+                    weakSelf.userAvatarImage = retAvatar;
+                    postNotification(RCInfoStringPostSuccess);
+                    [weakSelf.btnAvatarImg setBackgroundImage:retAvatar forState:UIControlStateDisabled];
+                }
+            });
+        }];
+    });
 }
 
 //- (void)processBackgroundThreadUploadInBackground:(UIImage *)avatarImage
